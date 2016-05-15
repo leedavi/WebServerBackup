01d05754fb7a812beb0d1e0ae2ac3c709c9c949f
leedavi <dcl@nevoweb.com>
2016-05-15 12:28:23 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Mail;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace WebServerBackUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BackupDB();
+                BackupWebSites();
+                IISSettings();
+                IISlogFiles();
+                CopyToAzure();
+                ResultFile("OK");
+            }
+            catch (Exception ex)
+            {
+                ResultFile(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+        static String GetSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                return "";
+            }
+        }
+
+        private static void IISlogFiles()
+        {
+            var clearIISLogFiles = GetSetting("ClearIISLogFiles");
+            if (clearIISLogFiles.ToLower() == "true")
+            {
+                Console.WriteLine("IIS Log Files");
+                var logFileFolder = GetSetting("LogFileFolder");
+                var logFileRetensionDays = GetSetting("LogFileRetensionDays");
+                ExecuteCommandSync("forfiles /p \"" + logFileFolder + "\" /s /m *.* /c \"cmd /c Del @path\" /d -" + logFileRetensionDays);
+            }
+        }
+
+        private static void IISSettings()
+        {
+            var iisSettingsBackup = GetSetting("IISSettingsBackup");            
+            var webZipFolder = GetSetting("WebZipFolder");
+            if (webZipFolder != "" && iisSettingsBackup.ToLower() == "true")
+            {
+                Console.WriteLine("IIS Settings");
+                if (File.Exists(webZipFolder + "\\iis_settings.zip"))
+                {
+                    File.Delete(webZipFolder + "\\iis_settings.zip");
+                } 
+
+                ExecuteCommandSync("%windir%\\system32\\inetsrv\\appcmd list apppool/config/xml > " + webZipFolder + "\\iis_apppools.xml");
+                ExecuteCommandSync("%windir%\\system32\\inetsrv\\appcmd list site/config/xml > " + webZipFolder + "\\iis_sites.xml");
+
+                ZipArchive zip = ZipFile.Open(webZipFolder + "\\iis_settings.zip", ZipArchiveMode.Create);
+                var file = webZipFolder + "\\iis_apppools.xml";
+                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                File.Delete(file);
+                file = webZipFolder + "\\iis_sites.xml";
+                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                File.Delete(file);
+
+                zip.Dispose();
+            }
+        }
+
+        static void BackupDB()
+        {
+            var dataBaseBackup = GetSetting("DataBaseBackup");
+            if (dataBaseBackup.ToLower() == "true")
+            {
+                Console.WriteLine("DB Backup");
+                var sqlServer = GetSetting("SqlServer");
+                var sqlAdmin = GetSetting("SqlAdmin");
+                var sqlAdminPass = GetSetting("SqlAdminPass");
+                var backupDBFolder = GetSetting("BackupDBFolder");
+                var backupDBZipPathName = GetSetting("BackupDBZipPathName");
+                var compressDBBackup = GetSetting("CompressDBBackup");
+
+                // remove any existing backup files.
+                var files1 = System.IO.Directory.GetFiles(backupDBFolder, "*.bak");
+                foreach (string file in files1)
+                {
+                    File.Delete(file);
+                }
+
+                Server myServer = new Server(sqlServer);
+
+                myServer.ConnectionContext.LoginSecure = false;
+                myServer.ConnectionContext.Login = sqlAdmin;
+                myServer.ConnectionContext.Password = sqlAdminPass;
+
+                myServer.ConnectionContext.Connect();
+
+                foreach (Database myDatabase in myServer.Databases)
+                {
+                    if (myDatabase.ID > 4 && myDatabase.Status == DatabaseStatus.Normal)
+                    {
+                        Backup bkpDBFull = new Backup();
+                        /* Specify whether you want to back up database or files or log */
+                        bkpDBFull.Action = BackupActionType.Database;
+                        bkpDBFull.Database = myDatabase.Name;
+                        bkpDBFull.Devices.AddDevice(backupDBFolder.TrimEnd('\\') + "\\" + myDatabase.Name + ".bak", DeviceType.File);
+
+                        bkpDBFull.SqlBackup(myServer);
+                    }
+                }
+
+
+                if (myServer.ConnectionContext.IsOpen) myServer.ConnectionContext.Disconnect();
+
+                if (compressDBBackup.ToLower() == "true" && backupDBZipPathName.ToLower().EndsWith(".zip"))
+                {
+                    File.Delete(backupDBZipPathName);
+
+                    // zip .bak files.
+                    var files = System.IO.Directory.GetFiles(backupDBFolder, "*.bak");
+                    ZipArchive zip = ZipFile.Open(backupDBZipPathName, ZipArchiveMode.Create);
+                    foreach (string file in files)
+                    {
+                        zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                        File.Delete(file);
+                    }
+                    zip.Dispose();
+                }
+            }
+
+        }
+
+        static void BackupWebSites()
+        {
+            var websiteBackup = GetSetting("WebsiteBackup");
+            if (websiteBackup.ToLower() == "true")
+            {
+                Console.WriteLine("Website Backup");
+                var webRootFolder = GetSetting("WebRootFolder");
+                var webZipFolder = GetSetting("WebZipFolder");
+
+                // remove any existing backup files.
+                var files1 = System.IO.Directory.GetFiles(webZipFolder, "*.zip");
+                foreach (string file in files1)
+                {
+                    File.Delete(file);
+                }
+
+                if (webZipFolder != "" && webRootFolder != "")
+                {
+                    var lp = 1;
+                    var websitelist = GetListOfWebsite(webRootFolder, new List<string>());
+                    foreach (var webfolder in websitelist)
+                    {
+                        Console.WriteLine(webfolder);
+                        ZipFile.CreateFromDirectory(webfolder, webZipFolder + "\\" + webfolder.Split('\\').Last() + lp + ".zip");
+                        lp += 1;
+                    }
+
+                }
+            }
+
+        }
+
+        private static List<String> GetListOfWebsite(String searchFolderPath, List<String> websitelist)
+        {
+            var filelist = Directory.GetFiles(searchFolderPath, "*.aspx");
+            if (filelist.Any())
+            {
+                foreach (var f in filelist)
+                {
+                    if (f.EndsWith("Default.aspx"))
+                    {
+                        websitelist.Add(searchFolderPath);
+                        return websitelist;
+                    }
+                }
+            }
+
+            var dirlist = Directory.GetDirectories(searchFolderPath);
+            foreach (var d in dirlist)
+            {
+                websitelist = GetListOfWebsite(d, websitelist);
+            }
+
+            return websitelist;
+        }
+
+        private static void CopyToAzure()
+        {
+            var copytoazure = GetSetting("copytoazure");
+            if (copytoazure.ToLower() == "true")
+            {
+                Console.WriteLine("Copy to Azure");
+
+                var storageConnectionString = GetSetting("StorageConnectionString");
+                var storageContainer = GetSetting("StorageContainer");
+
+                // Retrieve storage account from connection string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+                // Create the blob client.
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+
+                // Retrieve reference to a previously created container.
+                CloudBlobContainer container = blobClient.GetContainerReference(storageContainer);
+
+                // Create the container if it doesn't already exist.
+                container.CreateIfNotExists();
+
+
+                // copy DB backup file to webfolder so it's also copied to Azure
+                var compressDBBackup = GetSetting("CompressDBBackup");
+                var webZipFolder = GetSetting("WebZipFolder");
+                if (compressDBBackup.ToLower() == "true")
+                {
+                    var backupDBZipPathName =  GetSetting("BackupDBZipPathName");
+                    var copiedfilenamepath = webZipFolder.Trim('\\') + "\\" + Path.GetFileName(backupDBZipPathName);
+                    if (copiedfilenamepath != backupDBZipPathName)
+                    {
+                        if (File.Exists(copiedfilenamepath)) File.Delete(copiedfilenamepath);
+                        File.Copy(backupDBZipPathName, copiedfilenamepath);
+                    }
+                }
+
+                var websiteBackup = GetSetting("WebsiteBackup");
+                if (websiteBackup.ToLower() == "true")
+                {
+                    var retensionminutes = GetSetting("retensionminutes");
+                    var filelist = Directory.GetFiles(webZipFolder, "*.zip");
+                    if (filelist.Any())
+                    {
+                        foreach (var f in filelist)
+                        {
+                            Console.WriteLine("Copy Azure: " + DateTime.UtcNow + " " + f);
+                            // Retrieve reference to a blob named "myblob".
+                            CloudBlockBlob blockBlob = container.GetBlockBlobReference(Path.GetFileName(f));
+
+                            // Create or overwrite the "myblob" blob with contents from a local file.
+                            using (var fileStream = System.IO.File.OpenRead(f))
+                            {
+                                blockBlob.UploadFromStream(fileStream);
+                            }
+
+                            blockBlob.CreateSnapshot();
+
+                        }
+
+                        // purge snapshots
+                        var bloblist = container.ListBlobs(null, true, BlobListingDetails.Snapshots);
+                        foreach (IListBlobItem item in bloblist)
+                        {
+                            //you must cast this as a CloudBlockBlob 
+                            //  because blobItem does not expose all of the properties
+                            CloudBlockBlob theBlob = item as CloudBlockBlob;
+
+                            //Call FetchAttributes so it retrieves the metadata.
+                            theBlob.FetchAttributes();
+
+                            if (theBlob.IsSnapshot)
+                            {
+                                if (theBlob.SnapshotTime.Value.AddMinutes(Convert.ToInt32(retensionminutes)) < DateTime.UtcNow)
+                                {
+                                    if (theBlob.SnapshotTime.Value.Day == 1)
+                                    {
+                                        if (theBlob.SnapshotTime.Value.AddDays(32) < DateTime.UtcNow)
+                                        {
+                                            theBlob.Delete();
+                                        }
+                                    }
+                                    else
+                                    {
+                                        theBlob.Delete();
+                                    }
+
+                                }
+
+                            }
+
+
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        private static void ResultFile(String resultmsg)
+        {
+            var webZipFolder = GetSetting("WebZipFolder");
+            var copytoazure = GetSetting("copytoazure");
+            if (copytoazure.ToLower() == "true" && webZipFolder != "")
+            {
+                Console.WriteLine("Send Result File");
+
+                var resultfile = webZipFolder.Trim('\\') + "\\resultfile.txt";
+                if (File.Exists(resultfile)) File.Delete(resultfile);
+                File.WriteAllText(resultfile, resultmsg);
+
+
+                var storageConnectionString = GetSetting("StorageConnectionString");
+                var storageContainer = GetSetting("StorageContainer");
+
+                // Retrieve storage account from connection string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+                // Create the blob client.
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+
+                // Retrieve reference to a previously created container.
+                CloudBlobContainer container = blobClient.GetContainerReference(storageContainer);
+
+                // Create the container if it doesn't already exist.
+                container.CreateIfNotExists();
+
+
+                Console.WriteLine("Azure: " + DateTime.UtcNow + " " + resultfile);
+                // Retrieve reference to a blob named "myblob".
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(Path.GetFileName(resultfile));
+
+                // Create or overwrite the "myblob" blob with contents from a local file.
+                using (var fileStream = System.IO.File.OpenRead(resultfile))
+                {
+                    blockBlob.UploadFromStream(fileStream);
+                }
+            }
+        }
+
+
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Executes a shell command synchronously.
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="command">string command</param></span>
+        /// <span class="code-SummaryComment"><returns>string, as output of the command.</returns></span>
+        private static void ExecuteCommandSync(object command)
+        {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                Console.WriteLine(result);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # WebServerBackup
 Auto Backup DNN Website files, data and copy to Azure Storage
+
+What is WebServerBackUp
+-----------------------
+
+WebServerBackUp is a console application designed to run from the windows task manager or manually activated.
+It is designed to backup all the files required by DNN Websites on the server and make a versioned (snapshot) history of these files on Azure Storage.
+
+
+Functions
+---------
+
+#BackupDB();
+Creates a backup of ALL user databases in a SQL server instance. And then zips these into a single zip file. 
+
+#BackupWebSites();
+Searches for any subfolder from the root folder provided, that contains a "Default.aspx" file.
+All sub-folders containing a "Default.aspx" will be zipped into individual sub-folder named zip files.
+
+#IISSettings();
+Makes a copy of all IIS settings and zips them
+
+#IISlogFiles();
+Clears all IIS Logs files older than the date given.
+
+#CopyToAzure();
+Copies all the backup files to Azure and makes a snapshot.  
+Any snapshots older than the given retension time will be deleted.
+The retension time of snapshots made on the 1st of the month is set to 32 days.
+
+#ResultFile();
+Sends a results file to the azure container, indicating if the backup was successful, this can be checked to ensure the backup has worked.
+
+Help Links
+----------
+
+https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs
+
+
+** Explorer to deal with snapshot downloads and management.
+http://www.red-gate.com/products/azure-development/azure-explorer/
